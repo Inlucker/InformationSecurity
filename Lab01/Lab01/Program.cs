fd908f2d80235c8c54ff699fed103831db40da61
Inlucker <arseny707@mail.ru>
2022-09-20 22:20:26 +0300

    added lab1

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Management;
+using System.IO;
+
+namespace Lab01
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string processor_id = "";
+      try
+      {
+        ManagementObjectSearcher searcher =
+            new ManagementObjectSearcher("root\\CIMV2",
+            "SELECT * FROM Win32_Processor");
+
+        foreach (ManagementObject queryObj in searcher.Get())
+        {
+          processor_id = queryObj["ProcessorId"].ToString();
+        }
+      }
+      catch (ManagementException e)
+      {
+        Console.WriteLine("An error occurred while querying for WMI data: " + e.Message);
+        return;
+      }
+
+      string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/.Lab01/processorid";
+      string saved_processor_id = "";
+
+      if (File.Exists(path))
+      {
+        using (StreamReader sr = File.OpenText(path))
+        {
+          string s = "";
+          while ((s = sr.ReadLine()) != null)
+            saved_processor_id = s;
+        }
+      }
+
+      if (saved_processor_id != processor_id)
+      {
+        Console.WriteLine("You can't execute this programm on this computer");
+        return;
+      }
+
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
---
+++ Lab01/Lab01Installer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Management;
+using System.IO;
+using System.Text;
+
+namespace Lab01Installer
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        ManagementObjectSearcher searcher =
+            new ManagementObjectSearcher("root\\CIMV2",
+            "SELECT * FROM Win32_Processor");
+        string serial_number = "";
+
+        foreach (ManagementObject queryObj in searcher.Get())
+        {
+          Console.WriteLine("-----------------------------------");
+          Console.WriteLine("Win32_Processor instance");
+          Console.WriteLine("-----------------------------------");
+          Console.WriteLine("ProcessorId: {0}", queryObj["ProcessorId"]);
+
+          serial_number = queryObj["ProcessorId"].ToString();
+
+          Directory.CreateDirectory(System.Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/.Lab01");
+          string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/.Lab01/processorid";
+          using (FileStream fs = File.Create(path))
+          {
+            byte[] info = new UTF8Encoding(true).GetBytes(serial_number);
+            fs.Write(info, 0, info.Length);
+          }
+        }
+      }
+      catch (ManagementException e)
+      {
+        Console.WriteLine("An error occurred while querying for WMI data: " + e.Message);
+      }
+    }
+  }
+}
