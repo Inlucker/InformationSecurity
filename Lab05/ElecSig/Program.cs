8e03129f7536abd8c5160722e06017a93f52a96b
Inlucker <arseny707@mail.ru>
2022-10-21 03:20:00 +0300

    small fixes + tested with .zip files

--- Lab05/ElecSig/Program.cs
+++ Lab05/ElecSig/Program.cs
@@ -12,13 +12,15 @@ namespace ElecSig
     {
       ElecSig elecSig = new ElecSig();
       string file_name = "text.txt";
-      string sig_file_name = file_name + ".sig";
       string file_name2 = "text2.txt";
+      //file_name = "nginx1.zip";
+      //file_name2 = "nginx2.zip";
+      string sig_file_name = file_name + ".sig";
       byte[] sig = elecSig.makeSigForFile(file_name);
       elecSig.writeSigToFile(sig_file_name, sig);
       Console.WriteLine("Made signature \"" + sig_file_name + "\" for file \"" + file_name + "\"");
-      bool res1 = elecSig.checkSig("text.txt", "text.txt.sig");
-      bool res2 = elecSig.checkSig("text2.txt", "text.txt.sig");
+      bool res1 = elecSig.checkSig(file_name, sig_file_name);
+      bool res2 = elecSig.checkSig(file_name2, sig_file_name);
 
       Console.WriteLine("Signature from file \"" + sig_file_name + "\" for file \"" + file_name + "\" is " + res1);
       Console.WriteLine("Signature from file \"" + sig_file_name + "\" for file \"" + file_name2 + "\" is " + res2);
